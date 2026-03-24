79614449591495de8994cb52c6b533cb0b835682
bonhyuk <faucon130111@gmail.com>
2026-03-24 14:06:13 +0900

    Initial Commit

---
+++ HaengdongHagoTests/HaengdongHagoTests.swift
@@ -0,0 +1,16 @@
+//
+//  HaengdongHagoTests.swift
+//  HaengdongHagoTests
+//
+//  Created by bonhyuk on 3/24/26.
+//
+
+import Testing
+
+struct HaengdongHagoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
